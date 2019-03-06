9ece995c64378a9ec2477b79362a957f843e08a9
Stephen Truong Trung Hieu <trunghieu.truong.bit@gmail.com>
2019-03-06 21:13:08 +0700

    feat: add visualize expense with matplotlib

--- src/skills/ping.py
+++ src/skills/ping.py
@@ -1,10 +1,22 @@
+from datetime import datetime
+
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler
 
-from src.system.acl import restrict_admin
+from src.system.acl import restrict_admin, must_be_user
+from src.domains.chart import visualize_user_expense
+from src.system.database import session_scope
 
 @restrict_admin
-def ping(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.message.chat_id, text="pong")
+@must_be_user
+@session_scope
+def ping(update: Update, context: CallbackContext, session):
+    user_id = update.effective_user.id
+    chartpath = visualize_user_expense(user_id, session)
+    context.bot.send_photo(
+        update.message.chat_id, 
+        open(chartpath, "rb"), 
+        caption=f"Generate at {str(datetime.now())}",
+    )
 
 ping_handler = CommandHandler("ping", ping)
--- src/skills/report.py
+++ src/skills/report.py
@@ -1,21 +1,28 @@
-
 from sqlalchemy.sql import func
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler
 
 from src.models.record import Record
 from src.system.database import session_scope
+from src.system.acl import must_be_user
 
+@must_be_user
 @session_scope
 def report(update: Update, context: CallbackContext, session):
     user_id = update.effective_user.id
 
-    result = session.query(
-        func.sum(Record.amount).label("amount"),
-        func.count(Record.id).label("number"),
-    ) \
-        .filter_by(user_id=user_id) \
+    result = (
+        session.query(
+            func.sum(Record.amount).label("amount"),
+            func.count(Record.id).label("number"),
+        )
+        .filter_by(user_id=user_id)
         .first()
-    context.bot.send_message(update.message.chat_id, f"You have {result.amount} in the balance. Recorded from {result.number} records.")
+    )
+    context.bot.send_message(
+        update.message.chat_id,
+        f"You have {result.amount} in the balance. Recorded from {result.number} records.",
+    )
+
 
 report_handler = CommandHandler("report", report)
